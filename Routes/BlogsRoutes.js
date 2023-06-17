c8d7e54cb7d23ddbf45cb30a1fca96f198ef7f44
abdul-wajid-afridi <wajidafridi8554@gmail.com>
2023-06-17 18:57:30 +0500

    bug

--- Routes/BlogsRoutes.js
+++ Routes/BlogsRoutes.js
@@ -1,8 +1,8 @@
-import  Express  from "express";
+import Express from "express";
 import { getData, postData } from "../Controller/BlogsController.js";
 
-const routes=Express.Router();
-routes.post("/new/work",postData)
-routes.get("/new/work",getData)
+const routes = Express.Router();
+routes.post("/new/work", postData);
+routes.get("/new/work", getData);
 
 export default routes;
--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-import Express  from "express";
+import Express from "express";
 import mongoose from "mongoose";
 import routes from "./Routes/BlogsRoutes.js";
 
-const app= Express();
- app.use(Express.json)
-//  app.use(routes)
-const url='mongodb://localhost:27017/WorkWithAli'
-const port=9000;
+const app = Express();
+app.use(Express.json());
+app.use("/", routes);
+const url = "mongodb://localhost:27017/WorkWithAli";
+const port = 9000;
 
 mongoose.connect(url).then(() => {
-   console.log('database is successfully connected')
+  console.log("database is successfully connected");
 
-   app.listen(port,()=>console.log(`the server is ${port}`));
-
-})
-
+  app.listen(port, () => console.log(`the server is ${port}`));
+});
